07b67aea726f988a4a75f82dd741e2cce49f40ae
ohaiklochan <59032381+ohaiklochan@users.noreply.github.com>
2021-04-05 13:49:35 -0500

    routes edits

--- game_backend/config/routes.rb
+++ game_backend/config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   resources :answers
   resources :questions
   resources :users
+
+  get '/hiscores', to: 'users#hiscores'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
