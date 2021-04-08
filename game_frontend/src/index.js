f46f4809b23bbc3dd026eb1b93cc4a789d6f6e21
ohaiklochan <59032381+ohaiklochan@users.noreply.github.com>
2021-04-07 22:27:01 -0500

    index edit

---
+++ game_frontend/src/index.js
@@ -0,0 +1,19 @@
+const userFormContainer = document.getElementById("user_form_container")
+const userForm = userFormContainer.querySelector('#user_form')
+const questionContainer = document.getElementById('question_container')
+const welcome = document.getElementById('welcome')
+const scoreCardDiv = document.querySelector("#score_card")
+const endGameDiv = document.querySelector('#end_of_game_message')
+const hiScoresDiv = document.querySelector('#hiscores')
+const gameWindow = document.getElementById('game_window')
+
+const startBtn = welcome.querySelector('#go')
+const restartBtn = hiScoresDiv.querySelector('#start_over')
+const hiScoresBtn = endGameDiv.querySelector('#to_hiscores')
+
+let newGame = new Game
+
+User.listenForUserSubmit(newGame)
+newGame.listenForStart()
+newGame.listenForHiScores()
+newGame.listenForRestart()
