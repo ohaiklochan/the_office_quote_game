60cd44fb7fb3ad786959c079336ab34a07fd0d66
ohaiklochan <59032381+ohaiklochan@users.noreply.github.com>
2021-04-11 19:13:26 -0500

    updated seeds

--- game_backend/db/seeds.rb
+++ game_backend/db/seeds.rb
@@ -203,6 +203,556 @@ questions = [
             "Robert California",
             "Nellie"
         ]
+    },
+
+    {
+        difficulty: 3,
+        question: "Who Said This? : The only problem is whenever I try to make a taco, I get too excited and crush it.",
+        correct_answer: "Kevin",
+        incorrect_answers: [
+            "Clark",
+            "Pete",
+            "Andy"
+        ]
+    },
+
+    {
+        difficulty: 3,
+        question: "Who Said This? : I’m guessing Angela is the one in the neighborhood that gives the trick-or-treaters toothbrushes, pennies, walnuts.",
+        correct_answer: "Pam",
+        incorrect_answers: [
+            "Gabe",
+            "Oscar",
+            "Phyllis"
+        ]
+    },
+
+    {
+        difficulty: 1,
+        question: "Who Said This? : Besides having sex with men, I would say the Finer Things Club is the gayest thing about me.",
+        correct_answer: "Oscar",
+        incorrect_answers: [
+            "Ryan",
+            "Robert California",
+            "Michael Scott"
+        ]
+    },
+
+    {
+        difficulty: 2,
+        question: "Who Said This? : If I had to, I could clean out my desk in five seconds, and nobody would ever know that I’d ever been here. And I’d forget, too.",
+        correct_answer: "Ryan",
+        incorrect_answers: [
+            "Kelly",
+            "Andy",
+            "Erin"
+        ]
+    },
+
+    {
+        difficulty: 1,
+        question: "Who Said This? : Who is Justice Beaver?",
+        correct_answer: "Dwight",
+        incorrect_answers: [
+            "Jim",
+            "Gabe",
+            "Creed"
+        ]
+    },
+
+    {
+        difficulty: 2,
+        question: "Who Said This? : Sometimes I get so bored I just want to scream, and then sometimes I actually do scream. I just sort of feel out what the situation calls for.",
+        correct_answer: "Kelly",
+        incorrect_answers: [
+            "Kevin",
+            "Pete",
+            "Andy"
+        ]
+    },
+
+    {
+        difficulty: 2,
+        question: "Who Said This? : I find the mystery genre disgusting. I hate being titillated.",
+        correct_answer: "Angela",
+        incorrect_answers: [
+            "Meredith",
+            "Robert California",
+            "Nellie"
+        ]
+    },
+
+    {
+        difficulty: 2,
+        question: "Who Said This? : Mini-Cupcakes? As In The Mini Version Of Regular Cupcakes? Which Is Already A Mini Version Of Cake? Honestly, Where Does It End With You People?",
+        correct_answer: "Kevin",
+        incorrect_answers: [
+            "Kelly",
+            "Robert California",
+            "Nellie"
+        ]
+    },
+
+    {
+        difficulty: 1,
+        question: "Who Said This? : I say dance, they say, ‘How high?'",
+        correct_answer: "Michael",
+        incorrect_answers: [
+            "David Wallace",
+            "Robert California",
+            "Jan"
+        ]
+    },
+
+    {
+        difficulty: 1,
+        question: "Who Said This? : As a person who buys a lot of erotic cakes, it’s nice to be represented on one.",
+        correct_answer: "Phyllis",
+        incorrect_answers: [
+            "Meredith",
+            "Pam",
+            "Ryan"
+        ]
+    },
+
+    {
+        difficulty: 1,
+        question: "Who Said This? : I love inside jokes. I’d love to be a part of one someday.",
+        correct_answer: "Michael",
+        incorrect_answers: [
+            "Gabe",
+            "Jo",
+            "Kevin"
+        ]
+    },
+
+    {
+        difficulty: 2,
+        question: "Who Said This? : Last year, Creed asked me how to set up a blog. Wanting to protect the world from being exposed to Creed’s brain, I opened up a Word document on his computer and put an address at the top. I’ve read some of it. Even for the internet, it’s… pretty shocking.",
+        correct_answer: "Ryan",
+        incorrect_answers: [
+            "Jim",
+            "Clark",
+            "Toby"
+        ]
+    },
+
+    {
+        difficulty: 1,
+        question: "Ultimatums are key. Basically, nobody does anything for me anymore unless I threaten to kill myself.",
+        correct_answer: "Kelly",
+        incorrect_answers: [
+            "Phyllis",
+            "Erin",
+            "Nellie"
+        ]
+    },
+
+    {
+        difficulty: 2,
+        question: "Who Said This? : You’re the people’s princess! Diana was nothing!",
+        correct_answer: "Meredith",
+        incorrect_answers: [
+            "Kelly",
+            "Michael",
+            "Dwight"
+        ]
+    },
+
+    {
+        difficulty: 3,
+        question: "Who Said This? : I think it’s great that the company’s making a commercial, because not very many people have heard of us. I mean, when I tell people I work at Dunder Mifflin, they think that we sell mufflers or muffins or mittens or… and frankly, all of those sound better than paper, so I let it slide.",
+        correct_answer: "Jim",
+        incorrect_answers: [
+            "Darryl",
+            "Ryan",
+            "Stanley"
+        ]
+    },
+
+    {
+        difficulty: 3,
+        question: "Who Said This? : I’m always thinking one step ahead, like a carpenter that makes stairs.",
+        correct_answer: "Andy",
+        incorrect_answers: [
+            "Creed",
+            "Kevin",
+            "Dwight"
+        ]
+    },
+
+    {
+        difficulty: 2,
+        question: "Who Said This? : I can’t use Phyllis! Are you kidding me? The moment she steps off this bar, I’ll be launched into space! God, you’re so insensitive.",
+        correct_answer: "Dwight",
+        incorrect_answers: [
+            "Creed",
+            "Angela",
+            "Michael"
+        ]
+    },
+
+    {
+        difficulty: 2,
+        question: "Who Said This? : You Only Live Once? False. You Live Every Day. You Only Die Once.",
+        correct_answer: "Dwight",
+        incorrect_answers: [
+            "Creed",
+            "Michael",
+            "Jim"
+        ]
+    },
+
+    {
+        difficulty: 1,
+        question: "Who Said This? : I Wish There Was A Way To Know You’re In The Good Old Days Before You've Actually Left Them.",
+        correct_answer: "Andy",
+        incorrect_answers: [
+            "Kelly",
+            "Gabe",
+            "Erin"
+        ]
+    },
+
+    {
+        difficulty: 3,
+        question: "Who Said This? : If I were buying my coffin, I would get one with thicker walls so you couldn’t hear the other dead people.",
+        correct_answer: "Dwight",
+        incorrect_answers: [
+            "Jim",
+            "Robert California",
+            "Gabe"
+        ]
+    },
+
+    {
+        difficulty: 2,
+        question: "Who Said This? : One day Michael came in and complained about a speed bump on the highway. I wonder who he ran over then.",
+        correct_answer: "Jim",
+        incorrect_answers: [
+            "Toby",
+            "Oscar",
+            "Meredith"
+        ]
+    },
+
+    {
+        difficulty: 1,
+        question: "Who Said This? : Would I rather be feared or loved? Easy. Both. I want people to be afraid of how much they love me.",
+        correct_answer: "Michael Scott",
+        incorrect_answers: [
+            "Jo Bennett",
+            "Robert California",
+            "Jan Levinson-Gould"
+        ]
+    },
+
+    {
+        difficulty: 2,
+        question: "Who Said This? : It takes an advanced sense of humor. I don't expect everybody to understand.",
+        correct_answer: "Michael",
+        incorrect_answers: [
+            "Andy",
+            "Kevin",
+            "Oscar"
+        ]
+    },
+
+    {
+        difficulty: 1,
+        question: "Who Said This? : I got six numbers. One more would have been a complete telephone number.",
+        correct_answer: "Kevin",
+        incorrect_answers: [
+            "Creed",
+            "Gabe",
+            "Meredith"
+        ]
+    },
+
+    {
+        difficulty: 2,
+        question: "Who Said This? : Sorry I annoyed you with my friendship.",
+        correct_answer: "Andy",
+        incorrect_answers: [
+            "Jim",
+            "Pam",
+            "Kelly"
+        ]
+    },
+
+    {
+        difficulty: 1,
+        question: "Who Said This? : I am one of the few people who looks hot eating a cupcake.",
+        correct_answer: "Kelly",
+        incorrect_answers: [
+            "Toby",
+            "Ryan",
+            "Nellie"
+        ]
+    },
+
+    {
+        difficulty: 2,
+        question: "Who Said This? : I am aware the effect I have on women.",
+        correct_answer: "Charles",
+        incorrect_answers: [
+            "Michael",
+            "Robert California",
+            "Dwight"
+        ]
+    },
+
+    {
+        difficulty: 2,
+        question: "Who Said This? : I love catching people in the act. That's why I always whip open doors.",
+        correct_answer: "Dwight",
+        incorrect_answers: [
+            "Angela",
+            "Phyllis",
+            "Pam"
+        ]
+    },
+
+    {
+        difficulty: 2,
+        question: "Who Said This? : Everything I have I owe to this job. This stupid, wonderful, boring, amazing job.",
+        correct_answer: "Jim",
+        incorrect_answers: [
+            "Toby",
+            "Michael",
+            "Pam"
+        ]
+    },
+
+    {
+        difficulty: 3,
+        question: "Who Said This? : There's something about an underdog that really inspires the unexceptional.",
+        correct_answer: "Robert California",
+        incorrect_answers: [
+            "Jo Bennett",
+            "Charles",
+            "Nellie"
+        ]
+    },
+
+    {
+        difficulty: 3,
+        question: "Who Said This? : I am so tired of the Black Eyed Peas. It's rock n roll for people who don't like rock n roll. It's rap for people who don't like rap. It's pop for peole who don't like pop.",
+        correct_answer: "Robert California",
+        incorrect_answers: [
+            "Kelly",
+            "Kevin",
+            "Dwight"
+        ]
+    },
+
+    {
+        difficulty: 2,
+        question: "Who Said This? : In high school, I organized a walk out over standardized testing. Got over 500 students to just skip the SATs. At the last second I chickened out, took it anyway got a twelve twenty. Always regretted it... I feel lachrymose.",
+        correct_answer: "Andy",
+        incorrect_answers: [
+            "Ryan",
+            "David Wallace",
+            "Darryl"
+        ]
+    },
+
+    {
+        difficulty: 1,
+        question: "Who Said This? : I feel God in this Chili's tonight.",
+        correct_answer: "Pam",
+        incorrect_answers: [
+            "Jim",
+            "Phyllis",
+            "Michael"
+        ]
+    },
+
+    {
+        difficulty: 3,
+        question: "Who Said This? : Sometimes the clothes at Gap Kids are just too flashy.",
+        correct_answer: "Angela",
+        incorrect_answers: [
+            "Erin",
+            "Phyllis",
+            "Nellie"
+        ]
+    },
+
+    {
+        difficulty: 3,
+        question: "Who Said This? : You know, I thought this was an office, not the thunderdome.",
+        correct_answer: "Clark",
+        incorrect_answers: [
+            "Pete",
+            "Andy",
+            "Stanley"
+        ]
+    },
+
+    {
+        difficulty: 2,
+        question: "Who Said This? : You need to access your uncrazy side, otherwise maybe this thing has run its course.",
+        correct_answer: "Darryl",
+        incorrect_answers: [
+            "Kelly",
+            "Ryan",
+            "Creed"
+        ]
+    },
+
+    {
+        difficulty: 3,
+        question: "Who Said This? : Everyone I know who skis is dead.",
+        correct_answer: "D'Angelo",
+        incorrect_answers: [
+            "Jo Bennett",
+            "Robert California",
+            "David Wallace"
+        ]
+    },
+
+    {
+        difficulty: 3,
+        question: "Who Said This? : My last job was at a Taco Bell Express. But then it became a full Taco Bell and, I don't know, I couldn't keep up.",
+        correct_answer: "Erin",
+        incorrect_answers: [
+            "Ryan",
+            "Kelly",
+            "Andy"
+        ]
+    },
+
+    {
+        difficulty: 1,
+        question: "Who Said This? : You don't want to get on my bad side, I have seen some horrible things, I own over 200 horror movies.",
+        correct_answer: "Gabe",
+        incorrect_answers: [
+            "Pete",
+            "Clark",
+            "Creed"
+        ]
+    },
+
+    {
+        difficulty: 2,
+        question: "Who Said This? : He answers to Michael. Michael G. Scott. Michael J. Fox. Mr. Fox. The Incredible Mr. Fox.",
+        correct_answer: "Holly",
+        incorrect_answers: [
+            "Michael",
+            "Dwight",
+            "Kevin"
+        ]
+    },
+
+    {
+        difficulty: 3,
+        question: "Who Said This? : My psychiatrist thinks that I have some self-destructive tendencies, and that for once, I should indulge them.",
+        correct_answer: "Jan",
+        incorrect_answers: [
+            "Oscar",
+            "Pam",
+            "Nellie"
+        ]
+    },
+
+    {
+        difficulty: 3,
+        question: "Who Said This? : I’m a breast cancer survivor, close, personal friends with Nancy Pelosi, and Truman Capote and I slept with three of the same guys.",
+        correct_answer: "Jo Bennett",
+        incorrect_answers: [
+            "D'Angelo",
+            "Phyllis",
+            "Nellie"
+        ]
+    },
+
+    {
+        difficulty: 2,
+        question: "Who Said This? : I have one simple philosophy in business: if the seat is open, the job is open. It's how I came to briefly race a Formula One car. The three slowest laps ever recorded.",
+        correct_answer: "Nellie",
+        incorrect_answers: [
+            "Kelly",
+            "Jo Bennett",
+            "Robert California"
+        ]
+    },
+
+    {
+        difficulty: 2,
+        question: "Who Said This? : It just occurred to me that Andy has been calling me Plop for so long, he forgot my real name. Which is...",
+        correct_answer: "Pete",
+        incorrect_answers: [
+            "Clark",
+            "Gabe",
+            "Dwight"
+        ]
+    },
+
+    {
+        difficulty: 3,
+        question: "Who Said This? : Hazing is a fun way to show a new employee that she is not welcome or liked.",
+        correct_answer: "Toby",
+        incorrect_answers: [
+            "Dwight",
+            "Jim",
+            "Ryan"
+        ]
+    },
+
+    {
+        difficulty: 2,
+        question: "Who Said This? : Life is short. Drive fast and leave a sexy corpse. That’s one of my mottos.",
+        correct_answer: "Stanley",
+        incorrect_answers: [
+            "Ryan",
+            "Kevin",
+            "Kelly"
+        ]
+    },
+
+    {
+        difficulty: 3,
+        question: "Who Said This? : The Taliban is the worst. Great heroin though.",
+        correct_answer: "Creed",
+        incorrect_answers: [
+            "Ryan",
+            "Robert California",
+            "D'Angelo"
+        ]
+    },
+
+    {
+        difficulty: 1,
+        question: "Who Said This? : Why are all these people here? There’s too many people on this earth. We need a new plague.",
+        correct_answer: "Dwight",
+        incorrect_answers: [
+            "Pam",
+            "Creed",
+            "Gabe"
+        ]
+    },
+
+    {
+        difficulty: 2,
+        question: "Who Said This? : Holly is ruining Michael’s life. He thinks she is so special, and she’s so not. Her personality is like a 3. Her sense of humor is a 2. Her ears are like a 7 and a 4. Add it all up and what do you get? 16. And he treats her like she’s a perfect 40. It’s nuts.",
+        correct_answer: "Erin",
+        incorrect_answers: [
+            "Meredith",
+            "Pam",
+            "Phyllis"
+        ]
+    },
+
+    {
+        difficulty: 2,
+        question: "Who Said This? : Every so often, Jim dies of boredom.",
+        correct_answer: "Pam",
+        incorrect_answers: [
+            "Oscar",
+            "Karen",
+            "Kelly"
+        ]
     }
 ]
 
